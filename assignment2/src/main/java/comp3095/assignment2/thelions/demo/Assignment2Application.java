a0fea9cb070ed42423dd3b669c7710fbc184101f
onurkazman <52292004+onurkazman@users.noreply.github.com>
2020-11-06 14:15:38 -0500

    Add files via upload

---
+++ assignment2/src/main/java/comp3095/assignment2/thelions/demo/Assignment2Application.java
@@ -0,0 +1,13 @@
+package comp3095.assignment2.thelions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2Application.class, args);
+    }
+
+}
---
+++ assignment2/src/test/java/comp3095/assignment2/thelions/demo/Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3095.assignment2.thelions.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
